657cfb7e96d8bf87a06bba31da6f23a3ab4d3f11
krzysztofpodlaski <krzysztof@sftconsult.com>
2022-07-04 00:33:07 +0200

    switch beetwen ts->html->style

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,53 @@
 import * as vscode from "vscode";
-
+import { existsSync } from "fs";
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand("ng-switch.switch", () => {
     const path: string =
       vscode.window.activeTextEditor?.document.uri.fsPath || "";
     var pathArr: string[] = path.split("/");
-    const isInsideNgFile: boolean = pathArr[pathArr.length - 1].includes(
-      ".component"
-    );
+    const isInsideNgFile: boolean =
+      pathArr[pathArr.length - 1].includes(".component");
     if (!isInsideNgFile) {
       vscode.window.showWarningMessage(
-        "You are probably not inside an Angular component file"
+        "You are probably 2 not inside an Angular component file"
       );
       return;
     }
-    const fileNameArr: string[] = pathArr[pathArr.length - 1].split(".");
-    switch (fileNameArr[fileNameArr.length - 1]) {
-      case "ts":
-      case "css":
-      case "scss":
-      case "sass":
-      case "less":
-        fileNameArr[fileNameArr.length - 1] = "html";
+    const stylesExt = ["scss", "sass", "css", "less",];
+    const tsExt = ["ts"];
+    const htmlExt = ["html"];
+    const isTs = (ext: string) => tsExt.includes(ext);
+    const isHtmlExt = (ext: string) => htmlExt.includes(ext);
+    const isStyleExt = (ext: string) => stylesExt.includes(ext);
 
-        pathArr[pathArr.length - 1] = fileNameArr.join(".");
-        break;
-      case "html":
-      default:
-        pathArr[pathArr.length - 1] = pathArr[pathArr.length - 1].replace(
-          ".html",
-          ".ts"
-        );
-        break;
+    const fileNameArr: string[] = pathArr[pathArr.length - 1].split(".");
+
+    const currentFileExtension = fileNameArr[fileNameArr.length - 1];
+
+    if (isTs(currentFileExtension)) {
+      fileNameArr[fileNameArr.length - 1] = "html";
+      pathArr[pathArr.length - 1] = fileNameArr.join(".");
+    } else if (isHtmlExt(currentFileExtension)) {
+      const foundedExtension = stylesExt.find((ext) => {
+        const tempFileNameArr = [...fileNameArr];
+        tempFileNameArr[tempFileNameArr.length - 1] = ext;
+        const tempPathArr: string[] = [...pathArr];
+        tempPathArr[tempPathArr.length - 1] = tempFileNameArr.join(".");
+        return existsSync(tempPathArr.join("/"));
+      });
+      if (foundedExtension) {
+        fileNameArr[fileNameArr.length - 1] = foundedExtension as string;
+      } else {
+        fileNameArr[fileNameArr.length - 1] = "ts";
+      }
+      pathArr[pathArr.length - 1] = fileNameArr.join(".");
+    } else {
+      fileNameArr[fileNameArr.length - 1] = "ts";
+      pathArr[pathArr.length - 1] = fileNameArr.join(".");
     }
+
     var openFilePath: string = pathArr.join("/");
+    vscode.window.activeTextEditor?.hide();
     var vsCodeOpenPath: vscode.Uri = vscode.Uri.file(openFilePath);
     vscode.workspace.openTextDocument(vsCodeOpenPath).then((doc) => {
       vscode.window.showTextDocument(doc);
